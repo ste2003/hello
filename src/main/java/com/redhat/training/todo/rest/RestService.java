aeeecbf37ccb5d3ee73befc91817a663a764d4d2
ste2003 <sandraencina@hotmail.com>
2021-09-02 13:00:50 -0300

    testing rest

--- src/main/java/com/redhat/training/todo/rest/RestService.java
+++ src/main/java/com/redhat/training/todo/rest/RestService.java
@@ -4,6 +4,7 @@ import javax.ejb.EJB;
 import javax.faces.bean.RequestScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -19,7 +20,7 @@ public class RestService {
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public String sayHello(){
+	public String sayHelloRest(){
 		System.out.println("en get*********************");
 		return personService.sayHello("Ana");
 		
@@ -31,6 +32,24 @@ public class RestService {
 	public String sayBye(){
 		System.out.println("en bye*********************");
 		return personService.sayHello("Bye Ana");
+		
+	}
+	
+	@GET
+	@Path("{nombre}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String hello(@PathParam("nombre") String nombre){
+		System.out.println("en hello*********************");
+		return personService.sayHello(nombre);
+		
+	}
+	
+	@GET
+	@Path("/probando/{nombre}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String probando(@PathParam("nombre") String nombre){
+		System.out.println("en probando*********************");
+		return personService.sayHello(nombre);
 		
 	}
 }
--- src/main/java/com/redhat/training/todo/ui/PersonHello.java
+++ src/main/java/com/redhat/training/todo/ui/PersonHello.java
@@ -11,6 +11,15 @@ import com.redhat.training.todo.service.PersonService;
 public class PersonHello {
 	
 	String mensaje;
+	String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
 	@EJB
 	private PersonService personService;
